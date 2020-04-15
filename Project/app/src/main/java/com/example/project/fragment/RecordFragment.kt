982b78dee3313106435a60c8268947e3d31d1840
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-16 01:47:46 +0900

    Add：フラグメント間でViewModel経由でデータを受け渡し

--- Project/app/src/main/java/com/example/project/fragment/RecordFragment.kt
+++ Project/app/src/main/java/com/example/project/fragment/RecordFragment.kt
@@ -3,6 +3,7 @@ package com.example.project.fragment
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
@@ -14,18 +15,35 @@ class RecordFragment : Fragment(R.layout.fragment_record) {
 
     private lateinit var model: SharedViewModel
 
+    // 体脂肪を表示するHomeのTextViewを初期化
+    private lateinit var bodyWeight: TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 体脂肪を表示するHomeのTextView
+        bodyWeight = view.findViewById(R.id.bodyWeight)
 
         model = activity?.run {
             ViewModelProviders.of(this)[SharedViewModel::class.java]
         } ?: throw Exception("Invalid Activity")
 
-        // 登録ボタンからHomeへの画面遷移
+        // 登録ボタン
         val registerButton = view.findViewById<Button>(R.id.registerButton)
         registerButton.setOnClickListener {
+            // 登録ボタンからHomeへの画面遷移
             val action = RecordFragmentDirections.actionNavigationRecordToNavigationHome()
             findNavController().navigate(action)
+
+            // フォームに入力した値を反映
+            model.updateInformation()
+            // Home のTextViewに反映
+            register()
         }
+    }
+
+    // 入力した情報をHomeで表示するTextViewに格納
+    private fun register() {
+        bodyWeight.text = model.bodyWeight.toString()
     }
 }
--- Project/app/src/main/java/com/example/project/viewmodel/SharedViewModel.kt
+++ Project/app/src/main/java/com/example/project/viewmodel/SharedViewModel.kt
@@ -3,4 +3,10 @@ package com.example.project.viewmodel
 import androidx.lifecycle.ViewModel
 
 class SharedViewModel : ViewModel() {
+    var bodyWeight: Int = 58
+        private set
+
+    fun updateInformation() {
+        bodyWeight = 60
+    }
 }

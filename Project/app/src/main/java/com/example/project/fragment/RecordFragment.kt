e9f6984aaad3aadc7ba992cb5ad86565c4f40d8e
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-16 00:37:37 +0900

    Add：登録ボタンからHomeへの画面遷移

--- Project/app/src/main/java/com/example/project/fragment/RecordFragment.kt
+++ Project/app/src/main/java/com/example/project/fragment/RecordFragment.kt
@@ -2,8 +2,10 @@ package com.example.project.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.project.R
 import com.example.project.viewmodel.SharedViewModel
 
@@ -18,5 +20,12 @@ class RecordFragment : Fragment(R.layout.fragment_record) {
         model = activity?.run {
             ViewModelProviders.of(this)[SharedViewModel::class.java]
         } ?: throw Exception("Invalid Activity")
+
+        // 登録ボタンからHomeへの画面遷移
+        val registerButton = view.findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val action = RecordFragmentDirections.actionNavigationRecordToNavigationHome()
+            findNavController().navigate(action)
+        }
     }
 }

121872c520ac5946f1548319530b7f3340f9db2c
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-22 22:14:56 +0900

    Add：表示するフラグメントを管理するアダプター用のファイルを作成

---
+++ Project/app/src/main/java/com/example/project/PagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.project
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class PagerAdapter(fm: FragmentManager, private val fragmentList: List<Fragment>) :
+    FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    // 表示するフラグメントを制御する
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    // viewPagerにセットするコンテンツ(フラグメントリスト)のサイズ
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+}

95aebdf19bacd9401d638083d86dd6a4048d9aa5
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-03 13:46:51 +0900

    Add：カレンダーを選択してその日に入力した内容をTextViewに表示

--- Project/app/src/main/java/com/example/project/fragment/CalendarFragment.kt
+++ Project/app/src/main/java/com/example/project/fragment/CalendarFragment.kt
@@ -1,11 +1,14 @@
 package com.example.project.fragment
 
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.view.View
 import android.widget.CalendarView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import com.example.project.DBHelper
+import com.example.project.PhysicalRecordContract
 import com.example.project.viewmodel.SharedViewModel
 import com.example.project.R
 import java.text.SimpleDateFormat
@@ -44,6 +47,30 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar) {
             var dayOfMonth = arrangeFormatt(dayOfMonth)
             val date = "$year-$month-$dayOfMonth"
             model.dateDetail = date
+
+            val dbHelper = DBHelper(activity!!)
+
+            val db = dbHelper.readableDatabase
+
+            val projection = arrayOf(
+                BaseColumns._ID,
+                PhysicalRecordContract.PhysicalRecordEntry.COLUMN_NAME_BODY_WEIGHT,
+                PhysicalRecordContract.PhysicalRecordEntry.COLUMN_NAME_BODY_FAT_PERCENTAGE,
+                PhysicalRecordContract.PhysicalRecordEntry.COLUMN_NAME_CREATED_AT)
+
+            val dateBegin = model.dateDetail + " 23:59:59"
+            val dateEnd = model.dateDetail + " 00:00:00"
+
+            val sql = "select bodyWeight, bodyFatPercentage, createdAt from physicalRecord where createdAt <= ? and createdAt >= ?  order by _id desc limit 1;"
+            val cursor = db.rawQuery(sql, arrayOf(dateBegin, dateEnd))
+
+            with(cursor) {
+                while (moveToNext()) {
+                    model.bodyWeight = cursor.getString(0)
+                    model.bodyFatPercentage = cursor.getString(1)
+                    model.basalMetabolicRate = cursor.getString(2)
+                }
+            }
 
             // 画面遷移
             val action = CalendarFragmentDirections.actionNavigationCalendarToNavigationDate()

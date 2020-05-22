42cfcb8d6f869e26217aed1be8e3d2b5225f0af8
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-22 14:13:16 +0900

    Add：ViewModelに日付のフォーマットを追加

--- Project/app/src/main/java/com/example/project/viewmodel/SharedViewModel.kt
+++ Project/app/src/main/java/com/example/project/viewmodel/SharedViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.project.viewmodel
 
 import androidx.lifecycle.ViewModel
+import java.time.format.DateTimeFormatter
 
 class SharedViewModel : ViewModel() {
 
     var dateDetail: String = "" // 詳細表示する日付
+    var calendarDateFormatted: String = ""
 
     var todayData: Array<String?> = arrayOf("", "", "", "") // 今日の登録データ
     lateinit var  todayImageData: ByteArray // 今日登録した画像データ
 
+    // ホームでの日付表示用のフォーマット
+    val homeDateFormatter = DateTimeFormatter.ofPattern("M月d日")
 }
